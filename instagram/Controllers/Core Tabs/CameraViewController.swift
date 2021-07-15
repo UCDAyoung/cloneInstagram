e728461f0a0f79149d786a6df129da63434fdefb
UCDAyoung <qkr93643857@gmail.com>
2021-07-15 23:48:15 +0900

    cloneCoding Start

---
+++ instagram/Controllers/Core Tabs/CameraViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CameraViewController.swift
+//  instagram
+//
+//  Created by 박준영 on 2021/07/15.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
---
+++ instagram/Controllers/Core Tabs/ExploreViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ExploreViewController.swift
+//  instagram
+//
+//  Created by 박준영 on 2021/07/15.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
